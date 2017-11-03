5c0a1812233a3c69c5d3e015c9c3cb5c8fbde648
Fanny HUANG <fhuang@e3r3p20.42.fr>
2017-11-03 17:47:37 +0100

    mini clean

--- libft/ft_printf/src/ft_stoa_base.c
+++ libft/ft_printf/src/ft_stoa_base.c
@@ -6,7 +6,7 @@
 /*   By: fhuang <fhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/13 13:40:41 by fhuang            #+#    #+#             */
-/*   Updated: 2017/11/02 19:04:02 by fhuang           ###   ########.fr       */
+/*   Updated: 2017/11/03 16:54:40 by fhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,12 @@ static int	count_n(unsigned short n, int base)
 
 char		*ft_stoa_base(short n, int base, int is_uin, int *is_neg)
 {
-	char				pattern[17];
+	const char			pattern[] = "0123456789abcdef";
 	char				*ret;
 	unsigned short		uin;
 	int					len;
 	int					neg;
 
-	ft_strcpy(pattern, "0123456789abcdef");
 	neg = (base == 10 && n < 0) ? 1 : 0;
 	neg && is_uin == 0 ? ABS(n) : 1;
 	uin = (unsigned short)n;
